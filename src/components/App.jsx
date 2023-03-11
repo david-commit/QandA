b5977547a28dd198191c2944ecb9f9d347d8ee42
david-commit <davidondiege@gmail.com>
2023-03-11 22:15:28 +0300

    Styled the navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './NavBar'
 import { Route, Routes } from 'react-router-dom'
+import Login from './Login'
+import Register from './Register'
+import Home from './Home'
 
 function App() {
 
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
